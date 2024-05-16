fce2ec45fff7ffcf2a81909ed5700b732ef91ab1
notdeath <103466637+FudDeath@users.noreply.github.com>
2024-05-16 11:19:12 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ import util from 'util';
 
 const log = (x) => console.log(util.inspect(x, false, null, true));
 
-// Read environment variables
+// TODO SET THESE. Increase the budget and price if needed.
 const PRIVATE_KEY = process.env.ACC1_PRIVATE_KEY || '';
-const SEND_TRANSACTION_GAS_BUDGET = 10_000_000n;
-const TPS = 1000;
-const GET_GAS_COINS_GAS_BUDGET = BigInt(TPS) * 10_000_000n;
+const SEND_TRANSACTION_GAS_BUDGET = 15_000_000n;
+const GET_GAS_COINS_GAS_BUDGET = 15_000_000n;
 const GAS_PRICE = 1100n;
+const TPS = 513;
+const SEQUENT_TXS = 10n;
 const PING_INTERVAL = process.env.PING_INTERVAL * 1000;
+const USELESS_PKG = '0xcd7af24572133a6772fae2867d37dd65f817da917cb44a056ec38743211f66cc';
 
-const AMOUNTS = Array(TPS).fill(SEND_TRANSACTION_GAS_BUDGET);
+
+const AMOUNTS = Array(TPS).fill(SEND_TRANSACTION_GAS_BUDGET * SEQUENT_TXS);
 
 const suiClient = new SuiClient({ url: process.env.URL_OVERRIDE || getFullnodeUrl('testnet') });
 
@@ -33,7 +36,10 @@ const getGasCoins = async ({ suiClient, keyPair }) => {
   txb.setGasBudget(GET_GAS_COINS_GAS_BUDGET); // Adjust gas budget as necessary
   txb.setGasPrice(GAS_PRICE);
 
-  const results = txb.splitCoins(txb.gas, AMOUNTS);
+  const results = txb.splitCoins(
+    txb.gas,
+    AMOUNTS.map((x) => txb.pure.u64(x))
+  );
 
   AMOUNTS.forEach((_, index) => {
     txb.transferObjects([results[index]], keyPair.toSuiAddress());
@@ -53,12 +59,19 @@ const getGasCoins = async ({ suiClient, keyPair }) => {
 const sendTransaction = async ({ suiClient, keyPair, gasCoin }) => {
   const txb = new TransactionBlock();
 
-  // TODO Call SPAM HERE
-
   txb.setSender(keyPair.toSuiAddress());
   txb.setGasBudget(SEND_TRANSACTION_GAS_BUDGET); // Adjust gas budget as necessary
   txb.setGasPrice(GAS_PRICE);
   txb.setGasPayment([gasCoin]);
+
+  const object = txb.moveCall({
+    target: `${USELESS_PKG}::useless::new`,
+  });
+
+  txb.moveCall({
+    target: `${USELESS_PKG}::useless::destroy`,
+    arguments: [object],
+  });
 
   const bytes = await txb.build({ client: suiClient, limits: {} });
 
@@ -68,60 +81,90 @@ const sendTransaction = async ({ suiClient, keyPair, gasCoin }) => {
     transactionBlock: bytes,
     options: { showEffects: true },
   });
+
   return { result, startTime };
 };
 
 const sleepAsync = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
+const loop = async (data) => {
+  const promises = [];
+  for (const result of data) {
+    promises.push(
+      sendTransaction({
+        suiClient,
+        keyPair,
+        gasCoin: result[0].reference,
+      })
+    );
+  }
+
+  const remainingGasCoin = await Promise.all(promises);
+
+  log('Batch sent');
+
+  if (remainingGasCoin.length) await loop(remainingGasCoin);
+};
+
 const main = async () => {
   let totalGasFees = 0n;
   let totalTransactions = 0;
-  let startTime;
 
   while (true) {
-    const gasCoins = await getGasCoins({ suiClient, keyPair });
+    try {
+      const gasCoins = await getGasCoins({ suiClient, keyPair });
 
-    const promises = [];
-    startTime = performance.now();
+      log('Gas Coins Created');
 
-    for (const gas of gasCoins) {
-      promises.push(
-        sendTransaction({ suiClient, keyPair, gasCoin: gas.reference })
-      );
-    }
+      const promises = [];
+      const startTime = performance.now();
 
-    const results = await Promise.allSettled(promises);
-
-    // Extracting and logging the necessary information
-    const startTimes = results
-      .filter(result => result.status === 'fulfilled')
-      .map(result => result.value.startTime);
-
-    startTimes.forEach((startTime, index) => {
-      const date = new Date(startTime);
-      const formattedTime = `${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds().toString().padStart(3, '0')}`;
-      const result = results[index];
-      const confirmed = result.value.result.effects.status.status === 'success';
-      const digest = result.value.result.digest;
-      console.log(`Transaction ${index + 1} submitted at ${formattedTime} - Confirmation: ${confirmed}, Digest: ${digest}`);
-      if (confirmed) {
-        totalGasFees += BigInt(result.value.result.effects.gasUsed.computationCost) +
-                        BigInt(result.value.result.effects.gasUsed.storageCost) -
-                        BigInt(result.value.result.effects.gasUsed.storageRebate);
+      for (const gas of gasCoins) {
+        promises.push(
+          sendTransaction({ suiClient, keyPair, gasCoin: gas.reference })
+        );
       }
-    });
 
-    const minStartTime = Math.min(...startTimes);
-    const maxStartTime = Math.max(...startTimes);
-    const timeDelta = maxStartTime - minStartTime;
-    const transactionsPerSecond = TPS;
+      const results = await Promise.allSettled(promises);
 
-    const totalGasFeesInSUI = Number(totalGasFees) / 1_000_000_000; // Convert Mist to SUI
+      const fulfilledResults = results.filter(result => result.status === 'fulfilled' && !result.value.error);
+      if (fulfilledResults.length === 0) {
+        console.log(chalk.blue('No transactions were successfully submitted.'));
+        await sleepAsync(PING_INTERVAL);
+        continue;
+      }
 
-    console.log(chalk.blue(`Time delta between first and last transaction:`) + chalk.green(` ${timeDelta} milliseconds`));
-    console.log(chalk.blue(`Total gas fees:`) + chalk.green(` ${totalGasFeesInSUI} SUI, Transactions per second:`) + chalk.green(` ${transactionsPerSecond}`));
+      const startTimes = fulfilledResults.map(result => result.value.startTime);
 
-    await sleepAsync(PING_INTERVAL);
+      startTimes.forEach((startTime, index) => {
+        const date = new Date(startTime);
+        const formattedTime = `${date.getMinutes()}:${date.getSeconds()}.${date.getMilliseconds().toString().padStart(3, '0')}`;
+        const result = fulfilledResults[index].value.result;
+        const confirmed = result.effects.status.status === 'success';
+        const digest = result.digest;
+        console.log(`Transaction ${index + 1} submitted at ${formattedTime} - Confirmation: ${confirmed}, Digest: ${digest}`);
+        if (confirmed) {
+          totalGasFees += BigInt(result.effects.gasUsed.computationCost) +
+                          BigInt(result.effects.gasUsed.storageCost) -
+                          BigInt(result.effects.gasUsed.storageRebate);
+        }
+      });
+
+      const minStartTime = Math.min(...startTimes);
+      const maxStartTime = Math.max(...startTimes);
+      const timeDelta = maxStartTime - minStartTime;
+      const transactionsPerSecond = TPS;
+
+      const totalGasFeesInSUI = Number(totalGasFees) / 1_000_000_000; // Convert Mist to SUI
+
+      console.log(chalk.blue(`Time delta between first and last transaction:`) + chalk.green(` ${timeDelta} milliseconds`));
+      console.log(chalk.blue(`Total gas fees:`) + chalk.green(` ${totalGasFeesInSUI} SUI, Transactions per second:`) + chalk.green(` ${transactionsPerSecond}`));
+
+      await sleepAsync(PING_INTERVAL);
+    } catch (e) {
+      log(e);
+      await sleepAsync(PING_INTERVAL);
+    }
   }
 };
 
